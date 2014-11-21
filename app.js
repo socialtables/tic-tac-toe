a5c2a83ffc31f540f82aed256aa884c19b8e3db2
Will <will.horning@gmail.com>
2014-11-21 08:48:40 -0500

    basic game logic finished

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var jade = require('jade');
 var bodyParser = require('body-parser');
 var browserify = require('browserify-middleware')
 var MongoClient = require('mongodb').MongoClient;
+var io = require('socket.io')(http);
+
+
 
 app.set('view engine', 'jade');
 
@@ -15,18 +18,24 @@ app.use(bodyParser.json());
 // isn't sensitive information in this case so I've put it here for convenience.
 MONGO_URI = 'mongodb://foobar:foobar@ds053390.mongolab.com:53390/tic_tac_toe'
 
-// MongoClient.connect(MONGO_URI, function(err, db){
-//     db.collection('games').insert({foo: 'bar'}, function(err, result){
-//         if(err) console.log(err);
-//     });
-// });
+io.on('connection', function(socket){
+    socket.on('game_completed', function(outcome){
+        MongoClient.connect(MONGO_URI, function(err, db){
+            db.collection('games').insert(outcome, function(err, result){
+                if(err) console.log(err);
+            });
+            io.emit('score_update', outcome)
+        });
+    })
+
+})
 
 app.get(
     '/js/bundle.js', 
     browserify(
         './public/coffee/main.coffee', 
         {
-            transform: ['coffeeify'],
+            transform: ['coffee-reactify'],
             minify: true,
             precompile: true,
             cache: 'dynamic'
