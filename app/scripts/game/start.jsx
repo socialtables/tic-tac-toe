a4c5f9f8c941ff8aeb160124b4c5237b1b141717
Nick <ndecoursin@gmail.com>
2014-12-10 13:29:10 -0500

    Algorithm for winning

--- app/scripts/game/start.jsx
+++ app/scripts/game/start.jsx
@@ -2,35 +2,126 @@
 
 var React = require('react'),
     BackboneReactMixin = require('backbone-react-component'),
-    mountNode = $("#jumbotron")[0];
+    mountNode = $("#jumbotron")[0],
+    _ = require('underscore');
 //var Players = require('../collections/Players');
 //var Player = require('../collections/Player');
 
-var app = app || {};
-app.PLAYERO = 'playerO';
-app.PLAYERX = 'playerX';
+var app = {
+    PLAYERX: {
+        name: 'playerX',
+        wins: 0
+    },
+	PLAYERO: {
+		name: 'playerO',
+        wins: 0
+	}
+}
+
+app.winningSolutions = {
+    '0': [
+        [1,2], // row0
+        [3,6], // column0
+        [4,8] // diagonal
+    ],
+    '1': [
+        [0,2], // row0
+        [4,7] // column1
+    ],
+    '2': [
+        [0,1], // row0
+        [5,8], // column2
+        [6,4] // reverse diagonal
+    ],
+    '3': [
+        [0,6], // row1
+        [4,5], // column0
+    ],
+    '4': [
+        [3,5], // row1
+        [0,8], // column1
+        [6,2], // diagonal
+        [0,8] // reverse diagonal
+    ],
+    '5': [
+        [3,4], // row2
+        [2,8], // column2
+    ],
+    '6': [
+        [7,8], // row2
+        [0,3], // column0
+        [4,2] // reverse diagonal
+    ],
+    '7': [
+        [6,8], // row2
+        [1,4], // column1
+    ],
+    '8': [
+        [6,7], // row2
+        [2,5], // column2
+        [0,4] // diagonal
+    ]
+}
 
 var Game = React.createClass({
     mixins: [BackboneReactMixin],
+    updateScoreboard: function (player) {
+        player.wins = player.wins + 1;
+        return;
+    },
+    isGameOver: (function () {
+        app.PLAYERO.cells = [];
+        app.PLAYERX.cells = [];
+        return function (player, index) {
+            var gameOver = false,
+                cells = player['cells'];
+            cells.push(index);
+            var solutions = app.winningSolutions[index];
+            _.each(solutions, function (arr) {
+                if(_.contains(cells, arr[0]) && _.contains(cells, arr[1])) {
+                    gameOver = true;
+                }
+            });
+            if( gameOver) {
+                this.updateScoreboard(player);
+                alert('Game over ' + player.name + ' has won!');
+            }
+            return gameOver;
+        };
+    })(),
     getInitialState: function () {
+        var gameBoard = [];
+        for (var i = 0; i < 9; i++)
+        {
+            gameBoard.push('');
+        }
         return {
-            whoseTurn: app.PLAYERX,
-
+            whoseTurn: app.PLAYERX.name,
+            gameBoard: gameBoard
         }
     },
     handleCellClick: function (location) {
-        debugger;
-        //var collect = this.getCollection();
-        var whoseTurn = this.state.whoseTurn;
-        if (whoseTurn === app.PLAYERX) {
-            this.setState({whoseTurn: app.PLAYERO})
+        var player = this.state.whoseTurn;
+        var index = location[0] * 3 + location[1];
+        var gameBoard = this.state.gameBoard;
+        gameBoard[index] = player;
+        if (player === app.PLAYERX.name) {
+            this.isGameOver(app.PLAYERX, index);
+            this.setState({
+                whoseTurn: app.PLAYERO.name,
+                gameBoard: gameBoard
+            })
         } else {
-            this.setState({whoseTurn: app.PLAYERX})
+            this.isGameOver(app.PLAYERO, index, gameBoard);
+            this.setState({
+                whoseTurn: app.PLAYERX.name,
+                gameBoard: gameBoard
+            })
         }
     },
     render: function() {
-        var Rows, rows = [[0,[0,1,2]], [1,[0,1,2]], [2,[0,1,2]]];
-        Rows = rows.map(function (row) {
+        var Rows;
+        Rows = [0,1,2].map(function (row) {
             return <Row row={row} onCellClick={this.handleCellClick} whoseTurn={this.state.whoseTurn}/>
         }, this);
         return  (
@@ -46,19 +137,18 @@ var Row = React.createClass({
         lookup = ['top-row', 'middle-row', 'bottom-row'];
         return lookup[row];
     },
-    getBorder: function (rowName) {
-        if(rowName === 'top-row' || rowName === 'middle-row') {
+    getBorder: function (row) {
+        if(row === 0 || row === 1) {
             return border = <div className="border-top"></div> // Rename to border-bottom?
         }
         return '';
     },
     render: function () {
-        var rowNumber = this.props.row[0],
-            rowName = this.getRowClassName(rowNumber),
-            border = this.getBorder(rowName),
-            cells = this.props.row[1];
-        Cells = cells.map(function (cell) {
-            return <Cell onCellClick={this.props.onCellClick} location={[rowNumber, cell]} whoseTurn={this.props.whoseTurn}/>
+        var row = this.props.row,
+            rowName = this.getRowClassName(row),
+            border = this.getBorder(row);
+        Cells = [0,1,2].map(function (cell) {
+            return <Cell onCellClick={this.props.onCellClick} location={[row, cell]} whoseTurn={this.props.whoseTurn}/>
         }, this);
         return (
             <div className={"row " + rowName}>
@@ -86,10 +176,14 @@ var Cell = React.createClass({
         if (this.state.isSet) {
             return;
         }
-        var cell = this.refs.cell.getDOMNode();
-		$(cell).css('background-color', 'red');
-        $(cell).text(this.props.whoseTurn);
-        this.props.onCellClick({location: this.props.location});
+        var cell = this.refs.cell.getDOMNode(),
+            player = this.props.whoseTurn;
+        if (player === app.PLAYERX.name) {
+            $(cell).append('<div><img src="images/X.svg" /></div>');
+        } else {
+            $(cell).append('<div><img src="images/O.svg" /></div>');
+        }
+        this.props.onCellClick(this.props.location);
         this.setState({isSet: true});
     },
     render: function () {
@@ -97,7 +191,8 @@ var Cell = React.createClass({
 		className = "cell " + className + " col-xs-2 col-sm-2 col-md-2 col-lg-2";
 		return (
 			<div onClick={this.handleClick} className={className} ref="cell">
-                {this.state.data}
+                <div className="pointer">
+                </div>
             </div>
         );
     }
