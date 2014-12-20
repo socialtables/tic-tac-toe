45c7248df4e9eb81b33539482f9550fd3bb2773b
Johnathan Weisner <joweisner@gmail.com>
2014-12-20 17:41:36 -0500

    Load scripts from app.js local variable scripts

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,10 +20,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(__dirname + '/bower_components'));
+app.locals.scripts = [
+  '/jquery/dist/jquery.min.js', 
+  '/react/react-with-addons.min.js',
+  '/react/JSXTransformer.js'
+];
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
