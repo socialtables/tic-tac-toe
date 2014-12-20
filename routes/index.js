6f55ab8a419b93153ff68feaef48eb34f4e53614
Johnathan Weisner <joweisner@gmail.com>
2014-12-20 17:44:23 -0500

    Title now Tic Tac Tow

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Tic-Tac-Toe' });
+
+  res.render('index', { 
+    title: 'Tic Tac Toe'
+   });
+  
 });
 
 module.exports = router;
